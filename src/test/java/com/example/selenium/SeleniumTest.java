e793eb1b4a767476f1b1b3877cfdc91a951dee45
Ronni Kahalani <ronni.kahalani@gmail.com>
2022-03-28 15:54:09 +0200

    Added screenshot save

--- src/test/java/com/example/selenium/SeleniumTest.java
+++ src/test/java/com/example/selenium/SeleniumTest.java
@@ -1,16 +1,23 @@
 package com.example.selenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
+import java.time.LocalDate;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SeleniumTest {
@@ -41,6 +48,8 @@ public class SeleniumTest {
         addButton.click();
         addButton.click();
 
+        saveScreenshot(driver, "selenium-elements-" + LocalDate.now() +".png");
+
         int size = driver.findElements(By.xpath("//*[@class='added-manually']")).size();
         Assert.assertTrue( size == 2);
     }
@@ -63,9 +72,20 @@ public class SeleniumTest {
 
         String items = driver.findElement(By.xpath("//span[@class='shopping_cart_badge']")).getText();
 
+        saveScreenshot(driver, "selenium-shoppingcart-" + LocalDate.now() +".png");
+
         Assert.assertTrue( "1".equals(items));
     }
 
+    private void saveScreenshot(RemoteWebDriver driver, String fileName) {
+        File s = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        try {
+            String screenshotFileName = fileName;
+            FileUtils.copyFile(s, new File(screenshotFileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @AfterAll
     public void closeBrowser(){
